9e86890b71eebafc5911f4fa8f8b872b63f778d1
Timothy Jaeger <tim.jaeger@gmail.com>
2013-03-31 12:27:01 -0400

    Additional html block

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if(isset($_POST['submit'])) {
 		$mail->Password = "b1p0l4r!";
 		$mail->FromName = "Optimino Website";
 		$mail->Subject  = "From Optimino.com";
-		$mail->Body     = "Name:<br /> $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
+		$mail->Body     = "Name:<br /><br /> $name \n\n<br />Email:<br /><br /> $email \n\n<br />Subject:<br /><br /> $subject \n\n<br />Comments:<br /><br />\n $comments";
 		$mail->AltBody = "Name: $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
 		$mail->AddAddress('hello@optimino.com');
 
